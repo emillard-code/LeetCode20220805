fb871abd35f62ccea75e0192d5022de1b8f5592f
Emerson Millard <emillard.code@gmail.com>
2022-08-16 12:00:00 -0700

    Modified attempt class.

--- src/main/java/com/project/attempt/LeetCodeAppRevised.java
+++ src/main/java/com/project/attempt/LeetCodeAppRevised.java
@@ -1,4 +1,32 @@
 package com.project.attempt;
 
 public class LeetCodeAppRevised {
+
+    public static void main(String[] args) {
+
+        int[] array = { 1, 2, 3 };
+        System.out.println(method(array, 4));
+
+    }
+
+    public static int method(int[] array, int target) {
+
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] <= target) {
+                count = count + method(array, target - array[i]);
+            }
+
+            if (target == 0) {
+                return 1;
+            }
+
+        }
+
+        return count;
+
+    }
+
 }
