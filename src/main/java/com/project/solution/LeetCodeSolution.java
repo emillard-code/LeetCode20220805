8fe10cccc697794227466d29647f451b2e65b166
Emerson Millard <emillard.code@gmail.com>
2022-08-16 12:00:00 -0700

    Modified solution class.

--- src/main/java/com/project/solution/LeetCodeSolution.java
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -1,4 +1,32 @@
 package com.project.solution;
 
-public class LeetCodeSolution {
+class LeetCodeSolution {
+
+    public static void main(String[] args) {
+
+        int[] array = { 1, 2, 3 };
+        System.out.println(method(array, 4));
+
+    }
+
+    public static int method(int[] nums, int target) {
+
+        if (target == 0) {
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (target >= nums[i]) {
+                count += method(nums, target - nums[i]);
+            }
+
+        }
+
+        return count;
+
+    }
+
 }
